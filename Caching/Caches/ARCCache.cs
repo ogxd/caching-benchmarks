d2a5e81adbcea79d3016045d64ec748f1e377df0
Olivier Giniaux <oginiaux@smartadserver.com>
2022-12-16 11:22:37 +0100

    Working ARC implementation

--- Caching/Caches/ARCCache.cs
+++ Caching/Caches/ARCCache.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
 using System.Runtime.InteropServices;
 
 namespace Caching;
@@ -17,6 +15,7 @@ public class ARCCache<TKey, TValue> : ICache<TKey, TValue>
     private IndexBasedLinkedList<Entry> B2 => _lrus[2];
     private IndexBasedLinkedList<Entry> T2 => _lrus[3];
 
+    // Adaptive parameter
     private int _p;
 
     public string Name { get; set; }
@@ -49,12 +48,14 @@ public class ARCCache<TKey, TValue> : ICache<TKey, TValue>
             {
                 case 0: // Promote from B1 to T1
                     index.lru = 1;
+                    // Adapt
+                    var delta1 = B2.Count > B1.Count ? B2.Count / B1.Count : 1;
+                    _p = delta1 <= (MaximumEntriesCount - _p) ? _p + delta1 : MaximumEntriesCount;   
                     // Entry in B1 had no value, we should compute it
                     entry.value = factory(key);
                     Observer?.CountCacheMiss();
                     B1.Remove(index.index);
                     index.index = T1.AddLast(entry);
-                    _p =  Math.Min(MaximumEntriesCount, _p + Math.Max(B2.Count / B1.Count, 1));
                     break;
                 case 1: // Promote from T1 to T2
                     index.lru = 3;
@@ -63,11 +64,14 @@ public class ARCCache<TKey, TValue> : ICache<TKey, TValue>
                     break;
                 case 2: // Promote from B2 to T2
                     index.lru = 3;
+                    // Adapt
+                    var delta2 = B1.Count > B2.Count ? B1.Count / B2.Count : 1;
+                    _p = delta2 < _p ? _p - delta2 : 0; 
                     // Entry in B2 had no value, we should compute it
                     entry.value = factory(key);
                     Observer?.CountCacheMiss();
                     B2.Remove(index.index);
-                    index.index = T2.AddLast(entry);
+                    index.index = T2.AddLast(entry); 
                     break;
                 case 3: // Refresh position in T2
                     T2.Remove(index.index);
@@ -78,22 +82,16 @@ public class ARCCache<TKey, TValue> : ICache<TKey, TValue>
 
         if (T2.Count + T1.Count > MaximumEntriesCount)
         {
-            if (T1.Count > 0 && index.lru == 1)
+            int lruToEvictIndex;
+            if (T1.Count > 0 && (T1.Count > _p || (T1.Count == _p && index.lru == 3)))
             {
-                
+                lruToEvictIndex = 1;
             }
             else
             {
-                
+                lruToEvictIndex = 3;
             }
             
-            int lruToEvictIndex = index.lru ^ 2;
-            if (_lrus[lruToEvictIndex].Count == 0 || (lruToEvictIndex == 1 && _lrus[lruToEvictIndex].Count >= p)
-                lruToEvictIndex ^= 2;
-            
-            Debug.Assert(lruToEvictIndex == 1 || lruToEvictIndex == 3);
-            
-            // Evict T1 if new entry was added to T2 and the way around
             var lruToEvict = _lrus[lruToEvictIndex];
             var lruGhost = _lrus[lruToEvictIndex - 1];
             var entryToEvict = lruToEvict[lruToEvict.FirstIndex].value;
@@ -104,21 +102,20 @@ public class ARCCache<TKey, TValue> : ICache<TKey, TValue>
             indexToEvict.lru = lruToEvictIndex - 1;
         }
 
-        Trim(B1);
-        Trim(B2);
+        if (B1.Count > MaximumEntriesCount - _p)
+            Trim(B1);
+        
+        if (B2.Count > _p)
+            Trim(B2);
 
         return entry.value;
     }
 
     private void Trim(IndexBasedLinkedList<Entry> lru)
     {
-        if (lru.Count > 5 * MaximumEntriesCount) // Threshold to be defined
-        {
-            // Evict T1 if new entry was added to T2 and the way around
-            var keyToRemove = lru[lru.FirstIndex].value.key;
-            lru.Remove(lru.FirstIndex);
-            _perKeyMap.Remove(keyToRemove);
-        }
+        TKey keyToRemove = lru[lru.FirstIndex].value.key;
+        lru.Remove(lru.FirstIndex);
+        _perKeyMap.Remove(keyToRemove);
     }
 
     public void Clear()
@@ -134,12 +131,6 @@ public class ARCCache<TKey, TValue> : ICache<TKey, TValue>
     {
         public int lru;
         public int index;
-
-        public Index(int lru, int index)
-        {
-            this.lru = lru;
-            this.index = index;
-        }
     }
     
     private struct Entry
